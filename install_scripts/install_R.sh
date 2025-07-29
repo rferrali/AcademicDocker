758a12bb2e6039757a37df4a0c5fa6588be35366
Romain Ferrali <rferrali@gmail.com>
2025-07-29 20:25:03 +0100

    bugfix

--- install_scripts/install_R.sh
+++ install_scripts/install_R.sh
@@ -2,14 +2,14 @@
 set -e
 echo "Installing R..."
 
-curl -LO https://github.com/r-lib/rig/releases/download/latest/rig-linux-aarch64-latest.tar.gz \
-  && tar xz -C /usr/local -f rig-linux-aarch64-latest.tar.gz \
-  && rm rig-linux-aarch64-latest.tar.gz \
-  && rig add release
+curl -LO https://github.com/r-lib/rig/releases/download/latest/rig-linux-$TARGETARCH-latest.tar.gz
+tar xz -C /usr/local -f rig-linux-$TARGETARCH-latest.tar.gz
+rm rig-linux-$TARGETARCH-latest.tar.gz
+rig add release
 
-curl -LO https://quarto.org/download/latest/quarto-linux-arm64.deb \
-    && apt-get install -y ./quarto-linux-arm64.deb \
-    && rm quarto-linux-arm64.deb
+curl -LO https://quarto.org/download/latest/quarto-linux-$TARGETARCH.deb
+apt-get install -y ./quarto-linux-$TARGETARCH.deb
+rm quarto-linux-$TARGETARCH.deb
 
 R -e "pak::pkg_install(c('renv'))"
 R -e "pak::pkg_install(c('knitr', 'rmarkdown'))"
--- install_scripts/install_tex.sh
+++ install_scripts/install_tex.sh
@@ -6,10 +6,8 @@ echo "Installing TinyTeX..."
 curl -fsSL "https://yihui.org/tinytex/install-unx.sh" \
   | sh -s - --admin --no-path
 if [ "$TARGETARCH" = "amd64" ]; then
-    echo "Creating symlink for AMD64 (x86_64-linux)"
     ln -sf /root/.TinyTeX/bin/x86_64-linux /root/.TinyTeX/bin/current-arch
 elif [ "$TARGETARCH" = "arm64" ]; then
-    echo "Creating symlink for ARM64 (aarch64-linux)"
     ln -sf /root/.TinyTeX/bin/aarch64-linux /root/.TinyTeX/bin/current-arch
 else
     echo "Unknown architecture $TARGETARCH"
